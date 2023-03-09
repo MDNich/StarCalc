4be86fcdf4074cf4ab6d5a936caacddcedfc5f1a
Astro Kid <71262643+MDNich@users.noreply.github.com>
2023-03-09 14:37:10 -0500

    add

--- StarCalc/ViewController.swift
+++ StarCalc/ViewController.swift
@@ -49,8 +49,10 @@ class LumMagViewController: NSViewController {
         let whatGiven = whatToSolveFor.indexOfSelectedItem
         if(whatGiven == 2) { // FIND LUMINOSITY
             let L0 = 3.0128e28 // W
-            let Lsun = 3.9e26 // W
             let exp = -0.4*(input!+5-5*log10(dist!))
+            
+            let Lsun = 3.9e26 // W
+
             let lum = L0 * pow(10, exp)
             let lumFactor = lum / Lsun
             
@@ -197,6 +199,278 @@ class BinSysViewController: NSViewController {
 
 
 class RRLyraeViewController: NSViewController {
+    
+    @IBOutlet var periodField: NSTextField!
+    @IBOutlet var brightnessField: NSTextField!
+    @IBOutlet var distanceField: NSTextField!
+    
+    @IBOutlet var periodUnit: NSPopUpButton!
+    @IBOutlet var brightnessUnit: NSPopUpButton!
+    @IBOutlet var distanceUnit: NSPopUpButton!
+    
+    @IBAction func calculatePeriod(_ sender: Any?) {
+        let L0 = 3.0128e28 // W
+        let Lsun = 3.9e26 // W
+        
+        
+        var distance = 0.0
+        if distanceField.stringValue == "" {
+            var distTMP = Double(distanceField.stringValue)!
+            switch brightnessUnit.indexOfSelectedItem {
+            case 0:
+                distance = distTMP
+            case 1:
+                distance = distTMP/3.261563
+            case 2:
+                distance = 1/(distTMP/1000)
+            default:
+                distance = distTMP
+            }
+        }
+        
+        
+        var absmag = 0.0
+        
+        switch brightnessUnit.indexOfSelectedItem {
+        case 0:
+            //lum = L0 * pow(10, exp)
+            absmag = log10(Double(brightnessField.stringValue)!*Lsun/L0)
+        case 1:
+            absmag = log10(Double(brightnessField.stringValue)!/L0)
+        case 2:
+            absmag = Double(brightnessField.stringValue)! + 5 - 5*log10(distance)
+        case 3:
+            absmag = Double(brightnessField.stringValue)!
+        default:
+            absmag = Double(brightnessField.stringValue)!
+        }
+        
+        // M = −2.81 • log(P) − 1.43
+        // (M+1.43)/(-2.81) = log(P)
+        // P = 10^((M+1.43)/(-2.81))
+        
+        var periodDays = pow(10,(absmag + 1.43)/(-2.81))
+        var periodPrint = 0.0
+        switch periodUnit.indexOfSelectedItem {
+        case 0:
+            periodPrint = periodDays/365
+        case 1:
+            periodPrint = periodDays
+        case 2:
+            periodPrint = periodDays*24
+        case 3:
+            periodPrint = periodDays*3600*24
+        default:
+            periodPrint = periodDays
+        }
+        
+        periodField.stringValue = "\(periodPrint)"
+                
+    }
+    
+    func calculatePeriodNONIB() -> Double {
+        let L0 = 3.0128e28 // W
+        let Lsun = 3.9e26 // W
+        
+        
+        var distance = 0.0
+        if distanceField.stringValue == "" {
+            var distTMP = Double(distanceField.stringValue)!
+            switch brightnessUnit.indexOfSelectedItem {
+            case 0:
+                distance = distTMP
+            case 1:
+                distance = distTMP/3.261563
+            case 2:
+                distance = 1/(distTMP/1000)
+            default:
+                distance = distTMP
+            }
+        }
+        
+        
+        var absmag = 0.0
+        
+        switch brightnessUnit.indexOfSelectedItem {
+        case 0:
+            //lum = L0 * pow(10, exp)
+            absmag = log10(Double(brightnessField.stringValue)!*Lsun/L0)
+        case 1:
+            absmag = log10(Double(brightnessField.stringValue)!/L0)
+        case 2:
+            absmag = Double(brightnessField.stringValue)! + 5 - 5*log10(distance)
+        case 3:
+            absmag = Double(brightnessField.stringValue)!
+        default:
+            absmag = Double(brightnessField.stringValue)!
+        }
+        
+        // M = −2.81 • log(P) − 1.43
+        // (M+1.43)/(-2.81) = log(P)
+        // P = 10^((M+1.43)/(-2.81))
+        
+        var periodDays = pow(10,(absmag + 1.43)/(-2.81))
+        var periodPrint = 0.0
+        switch periodUnit.indexOfSelectedItem {
+        case 0:
+            periodPrint = periodDays/365
+        case 1:
+            periodPrint = periodDays
+        case 2:
+            periodPrint = periodDays*24
+        case 3:
+            periodPrint = periodDays*3600*24
+        default:
+            periodPrint = periodDays
+        }
+        
+        periodField.stringValue = "\(periodPrint)"
+        
+        return periodDays
+    }
+    
+    
+    @IBAction func calculateBrightness(_ sender: Any?) {
+        var periodDays = 0.0
+        switch periodUnit.indexOfSelectedItem {
+        case 0:
+            periodDays = Double(periodField.stringValue)! * 365
+        case 1:
+            periodDays = Double(periodField.stringValue)!
+        case 2:
+            periodDays = Double(periodField.stringValue)! / 24
+        case 3:
+            periodDays = Double(periodField.stringValue)! / 3600 / 24
+        default:
+            periodDays = Double(periodField.stringValue)!
+        }
+        
+        var Mag = -2.81 * log10(periodDays)-1.43
+        
+        var distance = 0.0
+        if distanceField.stringValue == "" {
+            var distTMP = Double(distanceField.stringValue)!
+            switch brightnessUnit.indexOfSelectedItem {
+            case 0:
+                distance = distTMP
+            case 1:
+                distance = distTMP/3.261563
+            case 2:
+                distance = 1/(distTMP/1000)
+            default:
+                distance = distTMP
+            }
+        }
+        
+        
+        var brightnessPrint = 0.0
+        switch brightnessUnit.indexOfSelectedItem {
+        case 3:
+            brightnessPrint = Mag
+        case 2:
+            brightnessPrint = Mag - 5 + 5*log10(distance)
+        case 0:
+            let Lsun = 3.9e26 // W
+            let L0 = 3.0128e28 // W
+            let exp = -0.4*(Mag)
+            let lum = L0 * pow(10, exp)
+            brightnessPrint = lum / Lsun
+        case 1:
+            let Lsun = 3.9e26 // W
+            let L0 = 3.0128e28 // W
+            let exp = -0.4*(Mag)
+            brightnessPrint = L0 * pow(10, exp)
+        default:
+            brightnessPrint = 0.0
+        }
+        
+        brightnessField.stringValue = "\(brightnessPrint)"
+    
+        
+    }
+    
+    func calculateBrightnessNONIB() -> Double {
+        var periodDays = 0.0
+        switch periodUnit.indexOfSelectedItem {
+        case 0:
+            periodDays = Double(periodField.stringValue)! * 365
+        case 1:
+            periodDays = Double(periodField.stringValue)!
+        case 2:
+            periodDays = Double(periodField.stringValue)! / 24
+        case 3:
+            periodDays = Double(periodField.stringValue)! / 24 / 3600
+        default:
+            periodDays = Double(periodField.stringValue)!
+        }
+        
+        var Mag = -2.81 * log10(periodDays)-1.43
+        
+        var distance = 0.0
+        if distanceField.stringValue == "" {
+            var distTMP = Double(distanceField.stringValue)!
+            switch brightnessUnit.indexOfSelectedItem {
+            case 0:
+                distance = distTMP
+            case 1:
+                distance = distTMP/3.261563
+            case 2:
+                distance = 1/(distTMP/1000)
+            default:
+                distance = distTMP
+            }
+        }
+        
+        
+        var brightnessPrint = 0.0
+        switch brightnessUnit.indexOfSelectedItem {
+        case 3:
+            brightnessPrint = Mag
+        case 2:
+            brightnessPrint = Mag - 5 + 5*log10(distance)
+        case 0:
+            let Lsun = 3.9e26 // W
+            let L0 = 3.0128e28 // W
+            let exp = -0.4*(Mag)
+            let lum = L0 * pow(10, exp)
+            brightnessPrint = lum / Lsun
+        case 1:
+            let Lsun = 3.9e26 // W
+            let L0 = 3.0128e28 // W
+            let exp = -0.4*(Mag)
+            brightnessPrint = L0 * pow(10, exp)
+        default:
+            brightnessPrint = 0.0
+        }
+        
+        brightnessField.stringValue = "\(brightnessPrint)"
+        
+        return Mag
+    }
+    
+    
+    @IBAction func calculateDistance(_ sender: Any?) {
+        print("calculating distance")
+        if(brightnessUnit.indexOfSelectedItem == 2) {
+            var absmag = calculateBrightnessNONIB()
+            var dist = pow(10,(Double(brightnessField.stringValue)!-absmag+5)/5)
+            switch brightnessUnit.indexOfSelectedItem {
+            case 0:
+                distanceField.stringValue = "\(dist)"
+            case 1:
+                distanceField.stringValue = "\(dist*3.261563)"
+            case 2:
+                distanceField.stringValue = "\(1/dist * 1000)"
+            default:
+                distanceField.stringValue = "\(dist) pc"
+            }
+        }
+        
+        else {
+            distanceField.stringValue = "PROVIDE M_APP"
+        }
+
+    }
     
     
 }
